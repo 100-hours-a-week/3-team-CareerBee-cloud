f6e7bd0c01bef3a954c4042861311c58a9a92079
j5hjun <johj1011@naver.com>
2025-05-23 14:46:34 +0900

    feat: terraform apply 시 최신 버전 배포 기능, DB 백업& 복원 스크립트, fix: 쿠키 도메인 -> dev.careerbee.co.kr

---
+++ transition/deploy/scripts/db-backup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+source ./app-variables.sh # cd deploy/scripts
+
+exec > >(gawk '{ print strftime("[%Y-%m-%d %H:%M:%S]"), $0; fflush(); }' >> ../logs/dp-backup.log) 2>&1
+
+VERSION_TAG=$(date +v%Y%m%d%H%M%S)
+FILE="backup-${VERSION_TAG}.sql"
+
+ssh -i $SSH_KEY ubuntu@$AWS_SERVER_IP << EOF
+mysqldump -u root -p"${DB_PASSWORD}" careerbee > $FILE
+
+aws s3 cp $FILE ${S3_BUCKET_INFRA}/db/
+
+rm $FILE
+EOF
