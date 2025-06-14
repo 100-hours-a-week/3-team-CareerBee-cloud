cf3fa91f22afa06d51b095c5af1648def27b6a91
j5hjun <johj1011@naver.com>
2025-06-14 22:27:58 +0900

    fix: 로그 그룹 생성, backend.tf 수정 [skip ci]

--- dev_always/main.tf
+++ dev_always/main.tf
@@ -93,3 +93,38 @@ resource "aws_ecr_repository" "ai_server" {
     Name        = "ecr-careerbee-dev-ai-server"
   }
 }
+
+resource "aws_cloudwatch_log_group" "backend" {
+  name              = "backend"
+  retention_in_days = 14
+}
+
+resource "aws_cloudwatch_log_group" "frontend" {
+  name              = "frontend"
+  retention_in_days = 14
+}
+
+resource "aws_cloudwatch_log_group" "vllm" {
+  name              = "vllm"
+  retention_in_days = 14
+}
+
+resource "aws_cloudwatch_log_group" "uvicorn" {
+  name              = "uvicorn"
+  retention_in_days = 14
+}
+
+resource "aws_cloudwatch_log_group" "nginx" {
+  name              = "nginx"
+  retention_in_days = 7
+}
+
+resource "aws_cloudwatch_log_group" "fluent-bit" {
+  name              = "fluent-bit"
+  retention_in_days = 3
+}
+
+resource "aws_cloudwatch_log_group" "mysql" {
+  name              = "mysql"
+  retention_in_days = 3
+}
