2099383577043fd1a7b88c8c468f9b2c88b865c3
j5hjun <johj1011@naver.com>
2025-06-14 15:25:21 +0900

    fix: s3 image to public [skip ci]

--- dev_always/main.tf
+++ dev_always/main.tf
@@ -44,6 +44,35 @@ resource "aws_s3_bucket_cors_configuration" "ssmu_bucket_image_cors" {
   }
 }
 
+resource "aws_s3_bucket_public_access_block" "ssmu_bucket_image_public_access" {
+  bucket                  = aws_s3_bucket.ssmu_bucket_image.id
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_policy" "ssmu_bucket_image_policy" {
+  bucket = aws_s3_bucket.ssmu_bucket_image.id
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Sid       = "AllowPublicRead",
+        Effect    = "Allow",
+        Principal = "*",
+        Action    = [
+          "s3:GetObject"
+        ],
+        Resource = "${aws_s3_bucket.ssmu_bucket_image.arn}/*"
+      }
+    ]
+  })
+
+  depends_on = [aws_s3_bucket_public_access_block.ssmu_bucket_image_public_access]
+}
+
 resource "aws_s3_bucket" "ssmu_bucket_infra" {
   bucket = var.s3_infra_bucket_name
   tags = var.s3_infra_bucket_tags
