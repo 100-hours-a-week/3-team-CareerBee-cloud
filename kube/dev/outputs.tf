f54dcd98609690c194dfb7a39aea5325879235ce
j5hjun <johj1011@naver.com>
2025-07-15 10:58:36 +0900

    fix: ALB 제거

--- kube/dev/outputs.tf
+++ kube/dev/outputs.tf
@@ -2,10 +2,6 @@ output "acm_arn" {
   value = data.aws_acm_certificate.careerbee_cert.arn
 }
 
-output "alb_name" {
-  value = aws_lb.alb.name
-}
-
 output "argocd_admin_password_hash" {
   value = var.argocd_admin_password_hash
   sensitive = true
